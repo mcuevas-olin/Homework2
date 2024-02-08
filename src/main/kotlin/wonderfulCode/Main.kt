c087e309f6df7d1a780dde492b15fc44830c532d
Ale <mcuevas@olin.edu>
2024-02-08 17:22:16 -0500

    Trying to Upload all files to github

--- src/main/kotlin/wonderfulCode/Main.kt
+++ src/main/kotlin/wonderfulCode/Main.kt
@@ -4,9 +4,5 @@ package wonderfulCode
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
-    var test = LinkedList<Int>()
-    test.pushFront(3)
-    println(test.peekFront())
-
+//placing this to try to upload.
 }
